7f7dbeec85c79cf36fa742eac829fce58ab1edee
kunshrestha <kunshrestha@hotmail.com>
2023-10-01 10:38:16 +1100

    Application class

--- src/main/java/com/optimed/appointmentmicroservice/AppointmentMicroserviceApplication.java
+++ src/main/java/com/optimed/appointmentmicroservice/AppointmentMicroserviceApplication.java
@@ -1,11 +1,18 @@
 package com.optimed.appointmentmicroservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableJpaRepositories(basePackages = "com.optimed.appointmentmicroservice.repository")
+@EntityScan(basePackages = "com.optimed.appointmentmicroservice.model")
 public class AppointmentMicroserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppointmentMicroserviceApplication.class, args);
 	}
